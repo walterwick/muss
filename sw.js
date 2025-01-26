ab50d563167d752a151953d25742d083d4b4915a
walter wick <83695885+walterwick@users.noreply.github.com>
2025-01-26 20:14:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,14 @@
-const CACHE_NAME = 'music-player-v2';
+const CACHE_NAME = 'music-player-v1';
 const OFFLINE_URLS = [
   '/',
   '/index.html',
-  '/placeholder-art.png',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'https://walterwick.de/pic/1e28t5wucm0b1.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(OFFLINE_URLS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
   );
 });
 
@@ -41,20 +27,8 @@ self.addEventListener('fetch', event => {
     );
   } else {
     event.respondWith(
-      caches.match(event.request).then(response =>
-        response || fetch(event.request).then(networkResponse => {
-          if (networkResponse.ok) {
-            caches.open(CACHE_NAME).then(cache => 
-              cache.put(event.request, networkResponse.clone())
-            );
-          }
-          return networkResponse;
-        }).catch(() => {
-          if (event.request.mode === 'navigate') {
-            return caches.match('/index.html');
-          }
-        })
-      )
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
     );
   }
 });
